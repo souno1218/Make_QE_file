56647fe59c8be4694b6191684875d5f4906988db
souno1218 <supinosaurusuz@gmail.com>
2025-03-09 01:33:17 +0900

    Ver 2.0

--- old/Make_QE_file/.ipynb_checkpoints/__init__-checkpoint.py
+++
@@ -1,5 +0,0 @@
-
-#from QE_auto import *
-from .qe_out2cif import qe_out2cif
-from .qe_out2in import qe_out2in
-from .cif2qe_in import cif2qe_in
--- old/Make_QE_file/__init__.py
+++
@@ -1,7 +0,0 @@
-
-#from QE_auto import *
-from .qe_out2cif import qe_out2cif
-from .qe_out2in import qe_out2in
-from .cif2qe_in import cif2qe_in
-
-#https://zenn.dev/h_waka/articles/67d0dd45d8dc50
